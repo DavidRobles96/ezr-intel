7e3b60c131014e7bfcd68298920a04ce0b8bc731
unknown <davidathiasrobles@gmail.com>
2024-11-24 18:15:23 -0500

    module func

--- public/integrations/scamalytics.js
+++ public/integrations/scamalytics.js
@@ -1,1 +1,12 @@
-const output = 4567;
+export async function run(data) {
+    const response = await fetch(`https://scamalytics.com/ip/${data.value}/`);
+    const text = await response.text();
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(text, 'text/html');
+    const rows = htmlDoc.querySelectorAll('tr');
+    const parsed = Array.from(rows)
+        .map(row => row.innerText.trim().split('\n').map(x => x.trim()).filter(x => x.length))
+        .filter(x => x.length === 2 && x[1] !== 'n/a')
+    const parsedObj = Object.fromEntries(parsed);
+    return { 'table': { 'title': 'IP Information', 'rows': parsedObj } }
+}
