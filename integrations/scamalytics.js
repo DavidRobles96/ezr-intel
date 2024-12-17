3aff480fb85c4cf0b028652a35697fb692b521e5
unknown <davidathiasrobles@gmail.com>
2024-12-16 22:44:36 -0500

    Updates

--- integrations/scamalytics.js
+++ integrations/scamalytics.js
@@ -1,12 +1,11 @@
-export async function run(data, fetcher) {
-    const response = await chrome.runtime.sendMessage({ type: 'fetch', url: `https://scamalytics.com/ip/${data.value}/`, options: { retries: 2 } });
-    const text = await response.text();
+export async function run(props) {
+    let response = await props.extensionFetch(`https://scamalytics.com/ip/${props.datapoint.value}/`);
     const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(text, 'text/html');
+    const htmlDoc = parser.parseFromString(response.text, 'text/html');
     const rows = htmlDoc.querySelectorAll('tr');
     const parsed = Array.from(rows)
-        .map(row => row.innerText.trim().split('\n').map(x => x.trim()).filter(x => x.length))
-        .filter(x => x.length === 2 && x[1] !== 'n/a')
+      .map(row => row.innerText.trim().split('\n').map(x => x.trim()).filter(x => x.length))
+      .filter(x => x.length === 2 && x[1] !== 'n/a')
     const parsedObj = Object.fromEntries(parsed);
     return { 'table': { 'title': 'IP Information', 'rows': parsedObj } }
 }
